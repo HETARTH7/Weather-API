6891008723f7caf12738255482d5ec53a8819f7e
unknown <hetarth002@gmail.com>
2022-10-21 13:46:49 +0530

    search city temp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import options from "../api";
 
 const App = () => {
+  const [temp, setTemp] = useState();
+  const [city, setCity] = useState("");
+  const [cityName, setCityName] = useState("jaipur");
+  const handleChange = (e) => {
+    setCity(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    setCityName(city);
+    e.preventDefault();
+  };
+  const options = {
+    method: "GET",
+    url: "https://weatherapi-com.p.rapidapi.com/current.json",
+    params: { q: cityName },
+    headers: {
+      "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+      "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+    },
+  };
+
   axios
     .request(options)
-    .then(function (response) {
-      console.log(response.data.current.temp_c);
+    .then((res) => {
+      setTemp(res.data.current.temp_c);
     })
-    .catch(function (error) {
-      console.error(error);
+    .catch((err) => {
+      console.error(err);
     });
 
-  return <div>App</div>;
+  return (
+    <div>
+      <br />
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} placeholder="Search City"></input>
+        <input type="submit"></input>
+      </form>
+      <h1>
+        Temperature in {cityName} is {temp}
+      </h1>
+    </div>
+  );
 };
 
 export default App;
